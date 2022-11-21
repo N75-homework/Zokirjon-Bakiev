1894cddc45df143a2ff33016ca51999cd1723234
bakiev83 <bakiyevzokirjon@gmail.com>
2022-11-21 11:29:43 +0500

    telNumber

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-let $toggleEye = document.querySelector('#toggleEye');
-let $inputPassword = document.querySelector('#inputPassword');
 
-$toggleEye.addEventListener('click', function (e) {
-    let typeInput = $inputPassword.getAttribute('type') === 'password' ? 'text' : 'password';
-    $inputPassword.setAttribute('type', typeInput);
-});
+let $inputForm = document.querySelector('#inputForm');
+let $inputTel = document.querySelector('#inputTel');
+let $inputBtn = document.querySelector("#inputBtn");
+let guitars = document.querySelector('aaa')
+;
+
+$inputForm.addEventListener('submit', function(evt) {
+    evt.preventDefault()
+    let $dateInfo = $inputTel.value.split('')
+    let arr =  $dateInfo.slice(0,9)
+    let str = arr.join('')
+    let result2 = str.padEnd(13, '#')
+    console.log('telNumber: ' + result2);
+    document.getElementById("aaa").innerHTML = 'telNumber: ' + result2;
+})
